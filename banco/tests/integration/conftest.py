3d87355f780c62a9af0d533270f92d64a1d9614c
DieglisonVarejao <dieglison.dev@gmail.com>
2026-02-10 23:28:01 -0400

    Add testes de integracao, primeira parte

---
+++ banco/tests/integration/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from src.app import create_app, db
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update(
+        {
+        "SECRET_KEY": "test",
+        "SQLALCHEMY_DATABASE_URI": "sqlite://",
+        "JWT_SECRET_KEY": "test",
+        }
+    )
+
+    with app.app_context():
+        db.create_all()
+        # other setup can go here
+        yield app
+        # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
